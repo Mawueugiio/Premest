6752d5d96275397702c606f61b461f98c46a102f
Eugenia Mawuenya Akpo <mawueugiio@gmail.com>
2020-10-06 08:10:06 +0000

    new components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import FirstComponent from './FirstComponnent';
 import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
 import './App.css';
 
 function App() { 
@@ -10,6 +11,7 @@ function App() {
       <p> This is our first custom component</p>
       <FirstComponent/>
       <SecondComponent/>
+      <ThirdComponent/>
     </div>
   );
 }
---
+++ src/ThirdComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ThirdComponent() {
+    return(
+        <>
+        <p><b><h2>About Me!</h2></b>
+            I am 22 and a computer Engineering student at Ashesi University. 
+            Nothing spectacular but once in my circle, you're bound to stay.
+            I like IG a lot but I haven't posted anything..funny right!
+        </p>
+        </>
+    )
+    }
+
+   export default ThirdComponent; 
